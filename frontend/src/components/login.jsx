9431b42aca01b7a2a2641f0944d2faa96620390c
solheredia <144463383+solheredia@users.noreply.github.com>
2024-06-20 18:15:23 -0300

    form

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,33 +1,22 @@
-import React from 'react';
+//import React from 'react';
 
 const Login = () => {
   return (
-   
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-6">
-            <div className="card mt-5">
-              <div className="card-body">
-                <div className="container mt-5">
-                  <form>
-                    <div className="mb-3">
-                      <label htmlFor="usuario" className="form-label">Usuario</label>
-                      <input type="usuario" className="form-control" id="usuario" />
-                    </div>
-                    <div className="mb-3">
-                      <label htmlFor="password" className="form-label">Password</label>
-                      <input type="password" className="form-control" id="password" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
+    <div>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="usuario" className="form-label">Usuario</label>
+          <input type="usuario" className="form-control" id="usuario" />
         </div>
-      </div>
-  
-   
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" id="password" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+
+
   );
 }
 
