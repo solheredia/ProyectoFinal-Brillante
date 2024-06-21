578d4a426b59fbe1dc3963e5bfe90e36a80620a6
solheredia <144463383+solheredia@users.noreply.github.com>
2024-06-21 10:38:26 -0300

    fronnt

--- frontend/src/components/createEmployee.jsx
+++ frontend/src/components/createEmployee.jsx
@@ -1,6 +1,15 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
+import axios from 'axios'
 
 export default class createEmployee extends Component {
+  state={
+    employee[]
+  }
+  async componentDidMount() {
+    const res = await axios.get('http://localhost:4000/api/employee')
+      this.setState({employee:res.data})
+      console.log(this.state.employee)
+  }
   render() {
     return (
       <div>
