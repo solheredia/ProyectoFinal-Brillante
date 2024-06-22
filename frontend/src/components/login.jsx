aa071ddb28cfee4c0dec82f476b43f2fc44cb819
solheredia <144463383+solheredia@users.noreply.github.com>
2024-06-22 16:31:35 -0300

    chauIf

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,39 +1,11 @@
 //import React from 'react';
 import '../styles/login.css';
 import { FaUser, FaUnlockKeyhole } from "react-icons/fa6";
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import CreateEmployee from './createEmployee';
+
 
 const Login = () => {
 
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [error,setError] =useState('');
-  const history = useHistory();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('/api/login', { name, password });
-      const { success, token, user } = response.data;
-      if (success) {
-        localStorage.setItem('token', token);
-        localStorage.setItem('user', JSON.stringify(CreateEmployee));
-        if (user.role === 'admin') {
-          history.push('/admin');
-        } else {
-          history.push('/employee');
-        }
-      }
-    } catch (error) {
-      console.error('Login failed:', error);
-      setError('Login failed.Please check your credentials and try again')
-      alert('Login failed. Please check your credentials and try again.');
-      
-    }
-  };
 
 
 
@@ -42,25 +14,23 @@ const Login = () => {
   return (
     <div className='wrapper'>
 
-      <form className='conteiner' onSubmit={handleLogin}>
+      <form className='conteiner' >
         <h2 className="title">Login Brillante</h2>
         <div className="input-box">
           <input
             className="box"
             type="text"
             placeholder='Usuario'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+
             required />
           <FaUser className='icon' />
 
         </div>
         <div className="input-box">
-          <input className='box' 
-          type="password"
-           placeholder='Password'
-           value={password}
-           onChange={(e) => setPassword(e.target.value)}
+          <input className='box'
+            type="password"
+            placeholder='Password'
+
             required />
           <FaUnlockKeyhole className='icon' />
 
@@ -70,7 +40,7 @@ const Login = () => {
           <a href="#">Forgot password?</a>
         </div>
         <button className="boton" type='submit'>Ingresar</button>
-        {error && <p className='error-message'>{error}</p>}
+
       </form>
     </div>
 
