0a6397ade6c3bccf462e165f140ee06733699ae9
solheredia <solheredia.555@gmail.com>
2024-06-27 20:38:09 -0300

    chatbotLISTOOOOOOOOOOO

--- frontend/src/components/empleado.jsx
+++ frontend/src/components/empleado.jsx
@@ -1,17 +1,40 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { format } from 'timeago.js';
-import{Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const EmployeePanel = () => {
   const [empleados, setEmpleados] = useState([]);
-//agrego
   const [filtroNombre, setFiltroNombre] = useState('');
   const [empleadosFiltrados, setEmpleadosFiltrados] = useState([]);
-  
+
   useEffect(() => {
     getEmployee();
+  }, []);
+
+  useEffect(() => {
+    const loadVoiceflowChatbot = () => {
+      const script = document.createElement('script');
+      script.src = 'https://cdn.voiceflow.com/widget/bundle.mjs';
+      script.type = 'text/javascript';
+      script.async = true;
+      script.onload = () => {
+        window.voiceflow.chat.load({
+          verify: { projectID: '667dde352a730391a8de5460' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production'
+        });
+      };
+      document.body.appendChild(script);
+
+      return () => {
+        // Remover el script cuando el componente se desmonte
+        document.body.removeChild(script);
+      };
+    };
+
+    loadVoiceflowChatbot();
   }, []);
 
   const getEmployee = async () => {
@@ -23,6 +46,7 @@ const EmployeePanel = () => {
       console.error('Error al obtener los empleados:', error);
     }
   };
+
   const handleInputChange = (e) => {
     setFiltroNombre(e.target.value);
     filterEmployees(e.target.value);
@@ -30,7 +54,7 @@ const EmployeePanel = () => {
 
   const filterEmployees = (filterValue) => {
     if (filterValue.trim() === '') {
-      setEmpleadosFiltrados(empleados); 
+      setEmpleadosFiltrados(empleados);
     } else {
       const filtered = empleados.filter((empleado) =>
         empleado.name.toLowerCase().includes(filterValue.toLowerCase())
@@ -39,11 +63,16 @@ const EmployeePanel = () => {
     }
   };
 
+ /* const openChatbot = () => {
+    if (window.voiceflow && window.voiceflow.chat) {
+      window.voiceflow.chat.open();
+    }
+  };*/
+
   return (
     <div>
-    <div>
       <div className="row">
-        <h3 className='text-white'>Panel Empleados</h3>
+        <h3 className="text-white">Panel Empleados</h3>
         <input
           type="text"
           className="form-control mb-3"
@@ -65,14 +94,16 @@ const EmployeePanel = () => {
           </div>
         ))}
       </div>
-    </div>
-    <div className="position-fixed top-0 end-0 m-3">
-      <div className="btn btn-outline-white">
-        <Link to='/ingresar' className="text-decoration-none text-white">
-          Cerrar Sesión
-        </Link>
+      <div className="position-fixed top-0 end-0 m-3">
+        <div className="btn btn-outline-white">
+          <Link to="/ingresar" className="text-decoration-none text-white">
+            Cerrar Sesión
+          </Link>
+        </div>
+        {/*<button className="btn btn-primary mt-3" onClick={openChatbot}>
+          Abrir Chatbot
+        </button>*/}
       </div>
-    </div>
     </div>
   );
 };
